c22a6f753fbfc66866ab7789f97e9fb44735835a
gregoryklein <gregoryhklein@gmail.com>
2015-12-05 21:01:46 -0500

    Added custom class for SubgoalsTableViewCell

---
+++ Goalie/SubgoalsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SubgoalsTableViewCell.swift
+//  Goalie
+//
+//  Created by Gregory Klein on 12/5/15.
+//  Copyright © 2015 Incipia. All rights reserved.
+//
+
+import UIKit
+
+class SubgoalsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
